740e62c4b9b0be5fa56eb5c0fb561cfcb3c98d9f
Nicholas Rogé <nicholas_roge@innovationfirst.com>
2018-07-18 08:09:53 -0500

    Added unsaved work.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import {hot} from 'react-hot-loader'
 
+import Layout from './Layout'
+import Block from './Layout/Block'
+import Reference from './Layout/Reference'
+
 
 const App = (props) => (
-    <div>Hello World</div>
+    <Layout>
+        <Reference name="example.1">
+            {block => block.append(
+                <div className="">Reference Appended Successfully</div>
+            )}
+        </Reference>
+
+        <div className="">
+            <Block name="example.1"
+        </div>
+        
+    </Layout>
 )
 
 
